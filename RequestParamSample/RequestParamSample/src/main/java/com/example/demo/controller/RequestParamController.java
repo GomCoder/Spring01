3c9435877c13b9b7c53551d1a8302851ab3bfccf
GomCoder <k2001911@hanmail.net>
2022-12-21 01:21:11 +0900

    chapter 07 요청 파라미터 취득하기

---
+++ RequestParamSample/RequestParamSample/src/main/java/com/example/demo/controller/RequestParamController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.form.Form;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+
+@Controller
+public class RequestParamController {
+    //입력화면을 표시
+    @GetMapping("show")
+    public String showView() {
+        //반환값으로 뷰 이름을 돌려줌
+        return "entry";
+    }
+
+    //확인화면을 표시
+    @PostMapping("confirm")
+    public String confirmView (
+            Model model, @RequestParam String name, @RequestParam Integer age,
+            @DateTimeFormat(iso =DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth
+    ) {
+        //Model에 저장
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        model.addAttribute("birth", birth);
+
+        //반환값으로 뷰 이름을 돌려줌
+        return "confirm";
+    }
+    //확인 화면을 표시: form 클래스용
+    public String confirmView(Form f) {
+        //반환값으로 뷰 이름을 돌려줌
+        return "confirm2";
+    }
+}
